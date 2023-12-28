07555144cf2b92d14e4e9da4beb217aab12a1436
TienPhuc02 <dtp2352002@gmail.com>
2023-12-28 11:20:29 +0700

    display category item on screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chapter_8/data/dummy_data.dart';
+import 'package:flutter_chapter_8/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
@@ -9,41 +12,21 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Pick Your Category"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            "1",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "2",
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            "3",
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            "4",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "5",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "6",
-            style: TextStyle(color: Colors.white),
-          ),
+        // children: availableCategories
+        //     .map(
+        //       (category) => CategoryGridItem(category: category),
+        //     )
+        //     .toList(),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
