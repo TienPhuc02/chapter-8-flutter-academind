18bcfe54b2bf53a30921d1e429d06efb9760f861
TienPhuc02 <dtp2352002@gmail.com>
2023-12-28 14:54:04 +0700

    add meal data

---
+++ lib/screens/meals.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chapter_8/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({Key? key, required this.title, required this.meals})
+      : super(key: key);
+  final String title;
+  final List<Meal> meals;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+        ),
+      ),
+      body: ,
+    );
+  }
+}
